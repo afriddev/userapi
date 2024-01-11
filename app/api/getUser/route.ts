67a9b1085fd40106a085033f94edfc27ff24d8ad
AFRID SHAIK <afridayan01@gmail.com>
2024-01-11 10:49:00 +0000

    commit

--- app/api/getUser/route.ts
+++ app/api/getUser/route.ts
@@ -1,22 +1,31 @@
 import connectDB from "@/libs/mongoDb/mongoDb";
 import userModel from "@/models/user";
 import { NextResponse } from "next/server";
-export  async function GET(){
+export  async function POST(req:Request){
 
     try{
         await connectDB()
-
-        const data = await userModel.find({})
+        const {email} = await req.json()
+        try{
+            const data = await userModel.findOne({email:email})
         return NextResponse.json({
             message:"success",
             data:data
         })
+
+        }
+        catch(e){
+            return NextResponse.json({
+                message:e.errors.email.name
+            })
+        }
+        
         
     }
     catch(e){
         
         return NextResponse.json({
-            message:e
+            message:e.message
         })
         
     }
