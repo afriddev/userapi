689f5cea06314bf8dc078ae2b76c47867b2c2fad
AFRID SHAIK <afridayan01@gmail.com>
2024-01-12 06:03:23 +0000

    commit

--- app/api/postUser/route.ts
+++ app/api/postUser/route.ts
@@ -31,6 +31,7 @@ export async function POST(req:Request){
                 return NextResponse.json({
                     message:e.errors
                 })
+
             }
         }
     }catch(e){
