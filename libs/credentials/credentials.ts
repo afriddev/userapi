0b66ebeabc188353b541a8d8be2043b235471fd1
AFRID SHAIK <afridayan01@gmail.com>
2024-01-16 08:51:58 +0000

    commit

--- libs/credentials/credentials.ts
+++ libs/credentials/credentials.ts
@@ -1,2 +1,2 @@
-const mongoDbUrl = "mongodb+srv://afridayan01:21765A0217@cluster0.umpwxcs.mongodb.net/?retryWrites=true&w=majority"
+const mongoDbUrl = "mongodb+srv://afridayan01:21765A0217@cluster0.umpwxcs.mongodb.net/users?retryWrites=true&w=majority"
 export default mongoDbUrl
